5f48e5e81428ff40cee754d3b5a02b16be5764f0
vitorozorio <vitormanoelbasilio357@gmail.com>
2023-09-04 21:02:00 -0300

    reformulando o codigo

--- Sistema_UMADEJ/src/br/com/UMADEJ/model/Jovem.java
+++ Sistema_UMADEJ/src/br/com/UMADEJ/model/Jovem.java
@@ -1,15 +1,11 @@
 package br.com.UMADEJ.model;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
+
 
 public class Jovem {
 
 	private int id; 
 	private String nome;
-	private Date dataCadastro;
-	private String dataNascimento;
+	private String idade;
 	private String dadosDoJovem;
 	
 	
@@ -25,49 +21,19 @@ public class Jovem {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public Date getDataCadastro() {
-		return dataCadastro;
-	}
-	public void setDataCadastro(Date dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
-	public String getDataNascimento() {
-		return dataNascimento;
-	}
-	public void setDataNascimento(String dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
+	
 	public String getDadosDoJovem() {
 		return dadosDoJovem;
 	}
 	public void setDadosDoJovem(String dadosDoJovem) {
 		this.dadosDoJovem = dadosDoJovem;
 	}
+	public String getIdade() {
+		return idade;
+	}
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
 	
-	public void setDataCadastro(String dataCadastro) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            this.dataCadastro = dateFormat.parse(dataCadastro);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-	public static void deleteByID(int i) {
-		// TODO Auto-generated method stub
-		
-	}
-	public static void update(Jovem jovem) {
-		// TODO Auto-generated method stub
-		
-	}
-	public static void save(Jovem jovem) {
-		// TODO Auto-generated method stub
-		
-	}
-	public static List<Jovem> getJovens() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	
 }
